e8f44e4029f133b91816e15e9378aba3cac42091
Harsh Patel <anonymousgrouphp@gmail.com>
2025-12-25 17:45:40 +0530

    feat(constants): unify CODATA fundamental physical constants and planetary parameters

---
+++ modules/solar_system_constants.js
@@ -0,0 +1,14 @@
+/**
+ * Solar System & Fundamental Physical Constants
+ */
+
+export const CONSTANTS = {
+    G: 6.67430e-11, // m^3 kg^-1 s^-2
+    SPEED_OF_LIGHT: 299792458, // m/s
+    ASTRONOMICAL_UNIT: 149597870700, // m
+    EARTH_RADIUS_KM: 6371.0,
+    EARTH_MASS_KG: 5.9722e24,
+    STANDARD_GRAVITY: 9.80665, // m/s^2
+    MOLAR_MASS_AIR: 0.0289644, // kg/mol
+    UNIVERSAL_GAS_CONSTANT: 8.314462618 // J/(mol*K)
+};
