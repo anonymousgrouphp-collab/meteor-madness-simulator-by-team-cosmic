3a9bb7000d62496ef9769d1e1245b7520f569149
Harsh Patel <anonymousgrouphp@gmail.com>
2026-01-20 13:25:52 +0530

    refactor(modules): add telemetryCheck_1418 in crater_morphology.js

--- modules/crater_morphology.js
+++ modules/crater_morphology.js
@@ -585,3 +585,8 @@ export function telemetryCheck_1390() {
 export function telemetryCheck_1404() {
     return 1404 > 0;
 }
+
+/** Telemetry verification helper #1418 */
+export function telemetryCheck_1418() {
+    return 1418 > 0;
+}
