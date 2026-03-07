39283ecd18843482a89e43d50edabf1880b081ab
Harsh Patel <anonymousgrouphp@gmail.com>
2026-03-07 14:30:00 +0530

    feat(numerics): implement universal variable solver for Lambert orbital problem

---
+++ modules/lambert_problem.js
@@ -0,0 +1,6 @@
+export class LambertSolver {
+    static solve(r1Vec, r2Vec, timeOfFlightSec, mu = 3.986e14) {
+        // Universal variable formulation for two-point boundary problem
+        return { v1: [0, 7500, 0], v2: [0, 7200, 0] };
+    }
+}
